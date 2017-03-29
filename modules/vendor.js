73bbd3dcfe34549aa2e657475be893ae38184da0
seieibob <joncole04412@gmail.com>
2017-03-29 01:24:22 -0400

    Remove internal jQuery dependency for Select2

--- modules/vendor.js
+++ modules/vendor.js
@@ -2,12 +2,12 @@
  * Use this file to import what you need from the bundled npm modules.
  */
 
-import 'qtip2';
+import "qtip2";
 //require("../node_modules/qtip2/dist/jquery.qtip.min.js");
 import "../node_modules/qtip2/dist/jquery.qtip.min.css";
 
-//require('select2');
-//require('../node_modules/select2/dist/js/select2.min.js');
+// Must import from node_modules folder or it won't see the shimmed jquery instance
+import '../node_modules/select2/dist/js/select2.min.js';
 import "../node_modules/select2/dist/css/select2.min.css";
 
 import "clip-path-polygon";
