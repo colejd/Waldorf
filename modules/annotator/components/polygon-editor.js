735acce1b6496dce115113052872e55ecc9fbb61
seieibob <joncole04412@gmail.com>
2017-03-31 19:48:23 -0400

    Disable persistent poly editor for now

--- modules/annotator/components/polygon-editor.js
+++ modules/annotator/components/polygon-editor.js
@@ -9,7 +9,7 @@
 class PolygonEditor {
     constructor(annotator){
         this.annotator = annotator;
-        this.baseZ = 2147483649 + 5;
+        this.baseZ = 2147483649;
         this.$breadcrumbs = [];
 
         // Create the video overlay
@@ -21,7 +21,7 @@ class PolygonEditor {
 
         // Create the poly object
         this.$poly = $("<div class='annotator-edit-poly'></div>").appendTo(this.annotator.player.$container);
-        this.$poly.css("z-index", this.$clickSurface.css("z-index") + 1);
+        this.$poly.css("z-index", this.baseZ + 1);
 
         this.ResizeOverlay();
         this.annotator.player.$container.on("OnFullscreenChange", (event, setFullscreen) => this.ResizeOverlay());
@@ -106,7 +106,7 @@ class PolygonEditor {
 
         $breadcrumb.css("left", (xPercent - (offPercentX / 2)).toString() + "%");
         $breadcrumb.css("top", (yPercent - (offPercentY / 2)).toString() + "%");
-        $breadcrumb.css("z-index", this.baseZ + 5);
+        $breadcrumb.css("z-index", this.baseZ - 50);
 
         
         $breadcrumb.draggable({ 
@@ -290,7 +290,7 @@ class PolygonEditor {
     }
 
     ShowJustPolygon(){
-        this.$poly.show();
+        //this.$poly.show();
         //this.UpdatePolyClipping();
     }
 
